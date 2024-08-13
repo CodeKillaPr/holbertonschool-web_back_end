d331e8bfa681d6a8ab1a6bc9402bd78ee667a9b8
killa <killa@killas-Laptop.local>
2024-08-13 16:09:36 -0400

    adding a extra docstring

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Type-annotated function concat that takes a string str1"""
+
 
 def concat(str1: str, str2: str) -> str:
     """Return the concatenation of two strings"""
