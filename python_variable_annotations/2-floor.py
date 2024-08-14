7a35dce47a5ab441b8a7c07e43fdd419cee8f921
killa <killa@killas-Laptop.local>
2024-08-14 11:10:12 -0400

    function floor which takes a float n as argument and returns the floor of the float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a function
+that returns the floor of the division of n by m"""
+
+
+def floor(n, m):
+    """Returns the floor of the division of n by m """
+    return n // m
